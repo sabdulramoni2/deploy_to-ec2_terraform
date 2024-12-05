d24ce79dbdb781d2c637d9f3347673b0ae81b264
sabdulramoni <abdulramonisaidi@yahoo.com>
2024-12-05 00:40:28 -0500

    update main.tf to add security_group

--- main.tf
+++ main.tf
@@ -5,6 +5,7 @@ variable vpc_cidr_blocks {}
 variable  subnet_cidr_block{}
 variable avail_zone {}
 variable env_prefix {}
+variable  my_ip {}
 
 resource "aws_vpc" "myapp-vpc" {
   cidr_block = var.vpc_cidr_blocks
@@ -23,7 +24,8 @@ resource "aws_subnet" "myapp-subnet-1" {
 }
 
 
-resource "aws_route_table" "myapp-route-table" {
+
+/* resource "aws_route_table" "myapp-route-table" {
   vpc_id = aws_vpc.myapp-vpc.id
 
   route {
@@ -34,7 +36,9 @@ resource "aws_route_table" "myapp-route-table" {
     Name: "${var.env_prefix}-rtb"
   }
 
-}
+} */
+
+
 
 resource "aws_internet_gateway" "myapp-igw" {
   vpc_id = aws_vpc.myapp-vpc.id
@@ -44,9 +48,53 @@ resource "aws_internet_gateway" "myapp-igw" {
 }
 
 
-resource "aws_route_table_association" "a-rtb-subnet" {
+/*resource "aws_route_table_association" "a-rtb-subnet" {
   subnet_id = aws_subnet.myapp-subnet-1.id
   route_table_id = aws_route_table.myapp-route-table.id
   
+}*/
+
+resource "aws_default_route_table" "main-rtb" {
+  default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id  
+  
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.myapp-igw.id
+  }
+  tags = {
+    Name: "${var.env_prefix}-main-rtb"
+  }
 }
 
+
+resource "aws_security_group" "myapp-sg" {
+  name = "myapp-sg"
+  vpc_id = aws_vpc.myapp-vpc.id
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "TCP"
+    cidr_blocks = [var.my_ip]
+  }
+
+  ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    prefix_list_ids = []
+  }
+
+  tags = {
+    Name: "${var.env_prefix}-sg"
+  }
+}
