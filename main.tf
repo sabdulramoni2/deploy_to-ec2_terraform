30bcd1ecc1070031ffbc78efce7137eec70deec8
sabdulramoni <abdulramonisaidi@yahoo.com>
2024-12-05 00:46:06 -0500

    update main.tf to use default  security_group

--- main.tf
+++ main.tf
@@ -68,8 +68,8 @@ resource "aws_default_route_table" "main-rtb" {
 }
 
 
-resource "aws_security_group" "myapp-sg" {
-  name = "myapp-sg"
+resource "aws_default_security_group" "default-sg" {
+  
   vpc_id = aws_vpc.myapp-vpc.id
 
   ingress {
@@ -95,6 +95,6 @@ resource "aws_security_group" "myapp-sg" {
   }
 
   tags = {
-    Name: "${var.env_prefix}-sg"
+    Name: "${var.env_prefix}-default-sg"
   }
 }
