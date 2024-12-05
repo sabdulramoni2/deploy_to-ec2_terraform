7a6f6bf6d109c21853d75c2ff8d934e4051d76a9
sabdulramoni <abdulramonisaidi@yahoo.com>
2024-12-04 20:08:22 -0500

    created VPC and subnet

---
+++ main.tf
@@ -0,0 +1,23 @@
+provider "aws" {
+  region = "us-east-2"
+}
+variable vpc_cidr_blocks {}
+variable  subnet_cidr_block{}
+variable avail_zone {}
+variable env_prefix {}
+
+resource "aws_vpc" "myapp-vpc" {
+  cidr_block = var.vpc_cidr_blocks
+  tags = {
+    Name: "${var.env_prefix}-vpc"
+  }
+}
+
+resource "aws_subnet" "myapp-subnet-1" {
+  vpc_id = aws_vpc.myapp-vpc.id
+  cidr_block = var.subnet_cidr_block
+  availability_zone = var.avail_zone
+  tags = {
+    Name: "${var.env_prefix}-subnet-1"
+  }
+}
