60dd73cb36bef934d898d8119a341eb21e441dff
sabdulramoni <abdulramonisaidi@yahoo.com>
2024-12-05 21:41:41 -0500

    add ec2 deployment config

--- main.tf
+++ main.tf
@@ -7,6 +7,7 @@ variable avail_zone {}
 variable env_prefix {}
 variable  my_ip {}
 variable  instance_type {}
+variable public_key_location {}
 
 resource "aws_vpc" "myapp-vpc" {
   cidr_block = var.vpc_cidr_blocks
@@ -117,6 +118,17 @@ output "aws-ami_id" {
   value = data.aws_ami.latest-amazon-linux-image.id
 }
 
+output "ec2-public_ip" {
+  value = aws_instance.myapp-server.public_ip
+}
+
+
+resource "aws_key_pair" "ssh-key" {
+  key_name = "server-key"
+  public_key = var.public_key_location
+}
+
+
 
 resource "aws_instance" "myapp-server" {
   ami = data.aws_ami.latest-amazon-linux-image.id
@@ -127,7 +139,12 @@ resource "aws_instance" "myapp-server" {
   availability_zone = var.avail_zone
 
   associate_public_ip_address = true
-  key_name = "server_key"
+  key_name = aws_key_pair.ssh-key.key_name
+
+
+  user_data = file("entry-script.sh")
+
+  user_data_replace_on_change = true
 
   tags = {
     Name: "${var.env_prefix}-server"
