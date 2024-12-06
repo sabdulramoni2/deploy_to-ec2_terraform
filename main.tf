80137f8fcbbd7df58435d67b608e104480784cf3
saidi abdulramoni <144086740+sabdulramoni2@users.noreply.github.com>
2024-12-06 00:24:28 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -142,31 +142,12 @@ resource "aws_instance" "myapp-server" {
   key_name = aws_key_pair.ssh-key.key_name
 
 
-  #user_data = file("entry-script.sh")
+  user_data = file("entry-script.sh")
 
   user_data_replace_on_change = true
 
 
-  connection {
-    type = "ssh"
-    host = self.public_ip
-    user = "ec2-user"
-    private_key = file(var.private_key_location)
-  }
-
-  provisioner "file" {
-    source = "entry-script.sh"
-    destination = "/home/ec2-user/entry-script-on-ec2.sh"
-  }
-
-  provisioner "remote-exec" {
-    inline = ["/home/ec2-user/entry-script.sh"]
-  }
-
-  provisioner "local-exec" {
-    command = "echo ${self.public_ip} > output.txt"
-  }
-
+  
 
   tags = {
     Name: "${var.env_prefix}-server"
